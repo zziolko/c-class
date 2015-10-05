dbf4f19617d3b7f54472926bafb2cffb480a1f1d
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-10-05 11:20:18 -0400

    rename roundoff example

--- A15-roundoff.c
+++ A15-roundoff.c
@@ -8,8 +8,8 @@
         http://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
 */
 int main(){
-    float a;
-    float b;
+    double a;
+    double b;
 
     for (int i=1; i<=50; i++){
         a += 0.9;
