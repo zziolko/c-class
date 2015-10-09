6e4637c677eb4bd58aec0e7c88cd0fe732fb2db5
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-10-09 10:11:32 -0400

    update makefile, fix structs bug, add a simple-makefile directory

--- A16-structures-1.c
+++ A16-structures-1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void updateParticleVelocities(x, y, z, u, v, w, dt) {
+void updateParticleVelocities(double x, double y, double z, double u, double v, double w, double dt) {
     x = x + dt*u;
     y = y + dt*v;
     z = z + dt*w;
@@ -9,12 +9,12 @@ void updateParticleVelocities(x, y, z, u, v, w, dt) {
 int main(){
     double x, y, z, u, v, w;
     double dt;
-    x1 = 0;
-    y1 = 0;
-    z1 = 0;
-    u2 = 2;
-    v2 = 3;
-    w2 = 4;
+    x = 0;
+    y = 0;
+    z = 0;
+    u = 2;
+    v = 3;
+    w = 4;
     dt = 0.1;
     updateParticleVelocities(x, y, z, u, v, w, dt);
 }
